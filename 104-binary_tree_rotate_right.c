1c7adc7963efb2497e31dd74357160862ee90149
A-fethi <fethiabderrahmane1@gmail.com>
2023-08-30 09:46:49 -0700

    performing a right-rotation on a binary tree

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree.
+ * @tree: a pointer to the root node of the tree to rotate.
+ * Return: a pointer to the new root node of the tree once rotated.
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t	*new;
+
+	new = tree->left;
+	tree->left = new->right;
+	new->right = tree;
+	if (tree->left != NULL)
+		tree->left->parent = tree;
+	new->parent = tree->parent;
+	tree->parent = new;
+	return (new);
+}
